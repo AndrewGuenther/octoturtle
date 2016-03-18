21f4a4a0a1cd85c9681eea6a43f965b2091ca19b
Andrew Guenther <guenther.andrew.j@gmail.com>
2016-03-17 22:30:37 -0700

    Payload tests don't make sense

--- test/payload-test.js
+++
@@ -1,9 +0,0 @@
-var Payload = require('../lib/payload.js');
-
-describe('A convenience wrapper for useful response fields', function() {
-  it('can retrieve the event action', function() {
-    var payload = Payload.extendPayload(buildResponse('issuesopened'));
-
-    expect(payload.getAction()).toBe('opened');
-  })
-})

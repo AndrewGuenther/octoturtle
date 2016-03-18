8a04c25582ecb35a44bd1c298ca980b7e0cd14b6
Andrew Guenther <guenther.andrew.j@gmail.com>
2016-03-17 19:31:19 -0700

    Added tests for Hook

---
+++ spec/hook-spec.js
@@ -0,0 +1,61 @@
+var Hook = require('../lib/hook.js');
+
+describe('A set of conditions and reactions to a payload',function() {
+  it('is a fluent interface', function() {
+    var hook = new Hook('issues');
+    expect(hook.is()).toBe(hook);
+    expect(hook.to()).toBe(hook);
+    expect(hook.by()).toBe(hook);
+    expect(hook.do()).toBe(hook);
+    expect(hook.when()).toBe(hook);
+  });
+
+  it('can respond to a specific event', function() {
+    var hook = new Hook('issues');
+    expect(hook.event).toBe('issues');
+  });
+
+  it('can respond to specific actions', function() {
+    var hook = new Hook('issues').is('created');
+    expect(hook.actions).toEqual(['created']);
+
+    hook.is('closed', 'reopened');
+    expect(hook.actions).toEqual(['created', 'closed', 'reopened']);
+  });
+
+  it('can respond to certain repositories', function() {
+    var hook = new Hook('issues').to('AndrewGuenther/octoturtle');
+    expect(hook.repositories).toEqual(['AndrewGuenther/octoturtle']);
+
+    hook.to('docker/docker', 'github/hubot');
+    expect(hook.repositories).toEqual(['AndrewGuenther/octoturtle', 'docker/docker',
+        'github/hubot']);
+  });
+
+  it('can respond to certain submitters', function() {
+    var hook = new Hook('issues').by('AndrewGuenther');
+    expect(hook.senders).toEqual(['AndrewGuenther']);
+
+    hook.by('warpling', 'Albatross');
+    expect(hook.senders).toEqual(['AndrewGuenther', 'warpling', 'Albatross']);
+  });
+
+  it('can perform actions unconditionally', function() {
+    var hook = new Hook('issues').do(function() {});
+    expect(hook.eval('issues', {})).toBe(true);
+  });
+
+  it('can perform actions only when certain conditions are met', function() {
+    var evaluated = false;
+    var hook = new Hook('issues').when(function() {return true;},
+        function() { evaluated = true; });
+    expect(hook.eval('issues', {})).toBe(true);
+    expect(evaluated).toBe(true);
+
+    evaluated = false;
+    hook = new Hook('issues').when(function() {return false;},
+        function() { evaluated = true; });
+    expect(hook.eval('issues', {})).toBe(false);
+    expect(evaluated).toBe(false);
+  });
+})

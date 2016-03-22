bdaa45f6caabf0911c440612651d5c0139619be7
Andrew Guenther <guenther.andrew.j@gmail.com>
2016-03-21 20:20:18 -0700

    More tests

---
+++ test/octoturtle-test.js
@@ -0,0 +1,19 @@
+var Octoturtle = require('../octoturtle.js');
+var Hook = require('../lib/hook.js');
+
+describe('A rule-based system for reacting to Github activity', function() {
+  it('provides a fluent interface', function() {
+    var hook = Octoturtle.whenA();
+    expect(hook instanceof Hook).toBe(true);
+    expect(Octoturtle.whenA).toBe(Octoturtle.whenAn);
+  });
+
+  it('provides an Express server out of the box', function() {
+    expect(Octoturtle.express).toBe(require('../deploy/express.js'));
+  });
+
+  it('provides a set of convenience function for interacting with Github',
+      function() {
+    expect(Octoturtle.Github).toBe(require('../lib/github.js'));
+  });
+});

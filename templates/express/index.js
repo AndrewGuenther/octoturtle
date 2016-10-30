706d4d77ba697bdf3415a1b275c87a5ffc7a90ac
Andrew Guenther <guenther.andrew.j@gmail.com>
2016-10-29 22:36:31 -0700

    Migrating to yeoman!

--- templates/express/index.js
+++
@@ -1,7 +0,0 @@
-var hooks = require('./hooks.js');
-var config = require('./config.json');
-var server = require('./server.js');
-
-server(hooks, config).listen(8080, '127.0.0.1', function() {
-  console.log('server has started');
-});
--- templates/lambda/octoturtle/index.js
+++
@@ -1,4 +0,0 @@
-var hooks = require('./hooks.js');
-var config = require('./config.json');
-
-exports.handler = require('./handler.js')(hooks, config);
